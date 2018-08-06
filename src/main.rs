7e86a959139411521dd5a81906fa9e042bcad7c5
Jon Poler <jonathan.poler@gmail.com>
2018-08-06 14:10:18 -0700

    Minimal configuration to get async/await from nightly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#![feature(async_await, await_macro, futures_api, tokio_compat)]
+
+#[macro_use]
+extern crate futures;
+
+use futures::{
+  FutureExt,
+  TryFutureExt,
+  compat::{
+    Future01CompatExt,
+    TokioDefaultExecutor,
+  },
+};
+use f1::{Future, Stream};
+use tokio::timer::Interval;
+use std::time::{Duration, Instant};
+
 fn main() {
-  println!("Hello, world!");
+  let future = async {
+    let timer = Interval::new(Instant::now(), Duration::from_secs(1))
+      .take(3)
+      .for_each(|_| {
+        println!("tick");
+        Ok(())
+      })
+      .map_err(|e| panic!("timer error: {}", e));
+
+    let join_handle = spawn_with_handle!(timer.compat()).unwrap();
+    await!(join_handle);
+  };
+
+  let future_compat = future
+    .boxed()
+    .unit_error()
+    .compat(TokioDefaultExecutor);
+
+  tokio::run(future_compat);
 }
