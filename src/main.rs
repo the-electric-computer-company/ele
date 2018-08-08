00965b1c61505a553fe73a1957087e5b51774ecb
Jon Poler <jpoler@users.noreply.github.com>
2018-08-07 19:31:40 -0700

    Thread me timbers (#9)
    Remove async code in favor of threads.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-#![feature(async_await, await_macro, futures_api, tokio_compat)]
-
-#[macro_use]
-extern crate futures;
-
-use f1::{Future, Stream};
-use futures::{
-  compat::{Future01CompatExt, TokioDefaultExecutor},
-  FutureExt, TryFutureExt,
-};
-use std::time::{Duration, Instant};
-use tokio::timer::Interval;
-
 fn main() {
-  let future = async {
-    let timer = Interval::new(Instant::now(), Duration::from_secs(1))
-      .take(3)
-      .for_each(|_| {
-        println!("tick");
-        Ok(())
-      }).map_err(|e| panic!("timer error: {}", e));
-
-    let join_handle = spawn_with_handle!(timer.compat()).unwrap();
-    await!(join_handle);
-  };
-
-  let future_compat = future.boxed().unit_error().compat(TokioDefaultExecutor);
-
-  tokio::run(future_compat);
+  println!("Hello threaded world!")
 }
